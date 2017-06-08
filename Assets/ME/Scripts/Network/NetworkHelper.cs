a54fa6f0447de8b2f2c122ea84ad026f0e2af581
Travis <tgpovey@gmail.com>
2017-06-08 13:45:55 +0800

    Add NetworkManager

---
+++ Assets/ME/Scripts/Network/NetworkHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+public static class NetworkHelper
+{
+    public enum PacketType : byte { CameraStream };
+
+    public static string GetLocalIPAddress()
+    {
+        if (NetworkInterface.GetIsNetworkAvailable())
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            throw new NetworkException("Local IP Address Not Found!");
+        }
+        else
+        {
+            throw new NetworkException("Not connected to a network");
+        }
+    }
+
+    public static byte[] FloatHostToNetworkOrder(float host)
+    {
+        byte[] bytes = BitConverter.GetBytes(host);
+
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(bytes);
+
+        return bytes;
+    }
+
+    public static float FloatNetworkToHostOrder(int network)
+    {
+        byte[] bytes = BitConverter.GetBytes(network);
+
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(bytes);
+
+        return BitConverter.ToSingle(bytes, 0);
+    }
+}
+
+public class NetworkException : Exception
+{
+    public NetworkException()
+    {
+    }
+
+    public NetworkException(string message)
+        : base(message)
+    {
+    }
+
+    public NetworkException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+    
+}
---
+++ Assets/ME/Scripts/Network/ServerManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class ServerManager : MonoBehaviour {
+
+
+    public int myPort = 54231;
+    public string myIpString;
+    private IPAddress localAddr;
+
+    private TcpListener listener = null;
+    private TcpClient client = null;
+    private NetworkStream stream = null;
+
+	// Use this for initialization
+	void Start () {
+        GetIPAddress();
+	}
+	
+    private bool GetIPAddress()
+    {
+        try
+        {
+            myIpString = NetworkHelper.GetLocalIPAddress();
+        }
+        catch (NetworkException e)
+        {
+            Debug.Log(e);
+            return false;
+
+        }
+        localAddr = IPAddress.Parse(myIpString);
+        return true;
+    }
+
+    // Begin listening for connections
+    public void BeginListening()
+    {
+        // If listener exists, remove it
+        if(listener != null)
+        {
+            Debug.Log("Listener exists");
+            try
+            {
+                listener.Stop();
+            }
+            catch { }
+            listener = null;
+        }
+
+        listener = new TcpListener(localAddr, myPort);
+        listener.Start();
+    }
+
+    // Accept a pending connection request
+    private void AcceptConnection()
+    {
+        client = listener.AcceptTcpClient();
+        stream = client.GetStream();
+    }
+
+    public void CloseConnection()
+    {
+        if(client != null)
+        {
+            client.Close();
+            client = null;
+            stream = null;
+        }
+    }
+
+    public void ReadPacket()
+    {
+        byte[] buf = new byte[5];
+
+        if (stream.Read(buf, 0, 5) != 5){
+            throw new NetworkException("Failed to read packet header");
+        }
+
+        NetworkHelper.PacketType pType = (NetworkHelper.PacketType) buf[0];
+        int pSize = IPAddress.NetworkToHostOrder(BitConverter.ToInt32(buf, 1));
+    }
+}
