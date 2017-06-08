0d0a42a434c83477ca5045141099461884868057
jamwhi <jamesgeewhite@gmail.com>
2017-06-08 14:20:32 +1000

    Decouple mesh from menu rows. Generate pic mesh dynamically.

--- Assets/ME/Scripts/MenuItem.cs
+++ Assets/ME/Scripts/MenuItem.cs
@@ -8,10 +8,18 @@ namespace CylinderMenu {
     public class MenuItem : MonoBehaviour {
 
         // Get an explicit refernce to the submenu transform so we can use it in dynamic menu creation
+		[HideInInspector]
         public List<MenuItem> subMenuItems;
 
+		[HideInInspector]
+		public MenuRow menuOnSelect;
+
+		public GameObject PicPrefab;
+
         public string itemName;
-        public MenuRow menuOnSelect;
+		public Texture menuPic;
+
+		private GameObject pic;
 
         // Initialize List
         private void Awake()
@@ -22,10 +30,15 @@ namespace CylinderMenu {
         private void Start()
         {
             // Get each MenuItem from the SubMenu container
-            foreach(Transform sub in transform.GetChild(0))
+            foreach(Transform sub in transform)
             {
                 subMenuItems.Add(sub.GetComponent<MenuItem>());
             }
+
+			pic = Instantiate(PicPrefab, transform).gameObject;
+			pic.GetComponent<Renderer>().material.mainTexture = menuPic;
+
+			gameObject.SetActive(false);
         }
     }
 }
