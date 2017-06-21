02f28e5aa1bbafc10db69c1185fbee7b461c1408
Travis <tgpovey@gmail.com>
2017-06-21 20:17:15 +0800

    PIP Assets added

---
+++ Assets/PIP/Handedness.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Handedness : MonoBehaviour {
+
+	Quaternion pointerPosition;
+	public float rightY=0;
+	public float leftY=-180;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+		public void LeftHanded (){
+		transform.localRotation=Quaternion.Euler (0, leftY, 0);
+
+		}
+
+		public void RightHanded (){
+		transform.localRotation=Quaternion.Euler (0, rightY, 0);
+
+	}
+	}
+
