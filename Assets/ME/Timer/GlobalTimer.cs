6b6451b72a6799e71048c432bb4999d29c316262
jamwhi <jamesgeewhite@gmail.com>
2017-06-26 13:03:19 +1000

    Fix rounding bug

--- Assets/ME/Timer/GlobalTimer.cs
+++ Assets/ME/Timer/GlobalTimer.cs
@@ -39,7 +39,8 @@ namespace CylinderMenu
 			{
 				currentTime -= Time.deltaTime;
 
-				if (currentDisplayNumber - currentTime > 1f)
+				Debug.Log((float)currentDisplayNumber - currentTime);
+				if ((float)currentDisplayNumber - currentTime >= 0f)
 				{
 					UpdateText();
 				}
@@ -54,8 +55,8 @@ namespace CylinderMenu
 
 		private void UpdateText ()
 		{
-			int mins = Mathf.FloorToInt(currentTime / 60f);
-			int secs = Mathf.CeilToInt(currentTime % 60f);
+			int mins = Mathf.FloorToInt((currentTime+0.99f) / 60f); // Adding 0.99 makes it so that it shows /rounds the higher number 
+			int secs = Mathf.FloorToInt((currentTime+0.99f) % 60f); // i.e. it won't hit 0 until actually at 0 (rather than as soon as it's below 1)
 
 			currentDisplayNumber = mins * 60 + secs;
 
