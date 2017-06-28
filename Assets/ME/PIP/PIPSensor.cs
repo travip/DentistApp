32ac5cbfa4f77bf15833fef8e903ef184b88c5f2
jamwhi <jamesgeewhite@gmail.com>
2017-06-28 16:22:53 +1000

    IP reverted to broadcast

--- Assets/ME/PIP/PIPSensor.cs
+++ Assets/ME/PIP/PIPSensor.cs
@@ -50,7 +50,7 @@ public class PIPSensor : MonoBehaviour {
         any = new IPEndPoint(IPAddress.Any, appPort);
         udpClient = new UdpClient(myPort);
         udpClient.EnableBroadcast = true;
-		broadcastAddr = new IPEndPoint(IPAddress.Parse("192.168.0.12"), 12345);
+		broadcastAddr = new IPEndPoint(IPAddress.Parse("255.255.255.255"), 12345);
 
         UdpState s = new UdpState();
         s.e = any;
