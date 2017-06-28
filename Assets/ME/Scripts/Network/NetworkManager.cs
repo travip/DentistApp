c6464b58662c5af0a7937531eec9132ea28e4ee2
Travis <tgpovey@gmail.com>
2017-06-28 14:36:26 +0800

    Auto stash before merge of "master" and "DentistApp/master"

--- Assets/ME/Scripts/Network/NetworkManager.cs
+++ Assets/ME/Scripts/Network/NetworkManager.cs
@@ -32,6 +32,7 @@ public class NetworkManager : MonoBehaviour {
     private IPEndPoint any;
 
     private bool discoveryingStream = false;
+    [SerializeField]
     private bool pipConnected = false;
     private bool camConnected = false;
 
@@ -221,18 +222,20 @@ public class NetworkManager : MonoBehaviour {
             float.Parse(gyro[2]),
             float.Parse(gyro[3]));
 
-        PIPController.instance.orientation = receivedQuart;
+        PIPController.instance.orientation = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * receivedQuart * new Quaternion(0, 0, 1, 0);
     }
-
+    
     public void ReceiveDatagram(IAsyncResult ar)
     {
-		Debug.Log("Got a datagram");
-		UdpClient u = ((UdpState)(ar.AsyncState)).u;
+        
+        UdpClient u = ((UdpState)(ar.AsyncState)).u;
         IPEndPoint e = ((UdpState)(ar.AsyncState)).e;
-		
-		byte[] recv = u.EndReceive(ar, ref e);
-		
-		switch (recv[0])
+
+        byte[] recv = u.EndReceive(ar, ref e);
+        Debug.Log("Got datagram");
+        Debug.Log("Leng: " + recv.Length);
+
+        switch (recv[0])
         {
             case PacketType.CAM_DISCOVERY:
                 if (discoveryingStream)
